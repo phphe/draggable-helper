c4eb3dd10e3994802eb8a318e5af19f0047b97b3
phphe <phphe@outlook.com>
2020-06-09 12:15:43 +0800

    good scroll box solution: edge scroll

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as hp from 'helper-js'
 // TODO
-// import DragEventService, {EventPosition, MouseOrTouchEvent,trackMouseOrTouchPosition} from 'drag-event-service'
-import DragEventService, {EventPosition, MouseOrTouchEvent,trackMouseOrTouchPosition} from '../../DragEventService'
+// import DragEventService, {EventPosition, MouseOrTouchEvent} from 'drag-event-service'
+import DragEventService, {EventPosition, MouseOrTouchEvent} from '../../DragEventService'
 
 /* Default export, a function.
 ```js
@@ -21,7 +21,11 @@ draggableHelper(listenerElement, options)
   listenerElement: HTMLElement. 绑定鼠标和触摸事件监听器的HTML元素.
   options: Options. 可选.
  */
-let dragging = false // Whether dragging is in progress. 拖拽是否正在进行中.
+const _edgeScroll = {
+  afterFirstMove(store: Store, opt: Options){},
+  afterMove(store: Store, opt: Options){},
+  afterDrop(store: Store, opt: Options){},
+}
 export default function (listenerElement: HTMLElement, opt:Options={}) {
   let store: Store
   // set default value of options
@@ -130,8 +134,8 @@ export default function (listenerElement: HTMLElement, opt:Options={}) {
     // calc move and attach related info to store
     // 计算move并附加相关信息到store
     const move = store.move = {
-      x: mouse.x - store.initialMouse.x,
-      y: mouse.y - store.initialMouse.y,
+      x: mouse.clientX - store.initialMouse.clientX,
+      y: mouse.clientY - store.initialMouse.clientY,
     }
     store.moveEvent = e
     store.mouse = mouse
@@ -150,7 +154,7 @@ export default function (listenerElement: HTMLElement, opt:Options={}) {
       }
       // resolve elements
       const movedElement = opt.clone ? movedOrClonedElement.cloneNode(true) as HTMLElement : movedOrClonedElement
-      const initialPosition = hp.getPosition(movedOrClonedElement)
+      const initialPosition = hp.getViewportPosition(movedOrClonedElement)
       // attach elements and initialPosition to store
       // 附加元素和初始位置到store
       store.movedOrClonedElement = movedOrClonedElement
@@ -168,7 +172,7 @@ export default function (listenerElement: HTMLElement, opt:Options={}) {
           height: `${Math.ceil(size.height)}px`,
           zIndex: 9999,
           opacity: 0.8,
-          position: 'absolute',
+          position: 'fixed',
           left: initialPosition.x + 'px',
           top: initialPosition.y + 'px',
           pointerEvents: 'none',
@@ -185,7 +189,6 @@ export default function (listenerElement: HTMLElement, opt:Options={}) {
       if (opt.beforeFirstMove && opt.beforeFirstMove(store, opt) === false) {
         return
       }
-      dragging = true
       if (opt.beforeMove && opt.beforeMove(store, opt) === false) {
         return
       }
@@ -194,6 +197,7 @@ export default function (listenerElement: HTMLElement, opt:Options={}) {
       if (!opt.updateMovedElementStyleManually) {
         store.updateMovedElementStyle()
       }
+      _edgeScroll.afterFirstMove(store, opt)
     } 
     // Not the first move
     // 非第一次移动
@@ -217,13 +221,13 @@ export default function (listenerElement: HTMLElement, opt:Options={}) {
         store.updateMovedElementStyle()
       }
     }
+    _edgeScroll.afterMove(store, opt)
     store.movedCount++
   }
 
   // define the event listener of mouseup and touchend
   // 定义mouseup和touchend事件监听器
   const onMouseupOrTouchEnd = (e: MouseOrTouchEvent) => {
-    dragging = false
     // cancel listening mousemove, touchmove, mouseup, touchend
     // 取消监听事件mousemove, touchmove, mouseup, touchend
     DragEventService.off(document, 'move', onMousemoveOrTouchMove, {touchArgs: [{passive: false}]})
@@ -253,7 +257,7 @@ export default function (listenerElement: HTMLElement, opt:Options={}) {
     if (!opt.updateMovedElementStyleManually) {
       updateMovedElementStyle()
     }
-    dragging = false
+    _edgeScroll.afterDrop(store, opt)
   }
 
   // define the destroy function
@@ -277,6 +281,9 @@ export const defaultOptions = {
   draggingClassName: 'dragging', // Be added to the dragged element. 将被添加到被拖动的元素.
   clone: false, // Whether to clone element when drag.
   updateMovedElementStyleManually: false, // If true, you may need to call store.updateMovedElementStyle in beforeFirstMove, beforeMove, beforeDrop
+  edgeScrollTriggerMargin: 50,
+  edgeScrollSpeed: 0.35,
+  edgeScrollTriggerMode: 'top_left_corner',
 }
 export interface Options extends Partial<typeof defaultOptions>{
   triggerClassName?: string|string[] // triggerElement must have the class name.
@@ -286,6 +293,11 @@ export interface Options extends Partial<typeof defaultOptions>{
   beforeFirstMove?: (store:Store, opt:Options) => boolean|undefined
   beforeMove?: (store:Store, opt:Options) => boolean|undefined
   beforeDrop?: (store:Store, opt:Options) => boolean|undefined
+  // edge scroll
+  edgeScroll?: boolean
+  edgeScrollTriggerMargin?: number
+  edgeScrollSpeed?: number
+  edgeScrollTriggerMode?: 'top_left_corner'|'mouse'
 }
 // Info after event triggered. Created when mousedown or touchstart, destroied after mouseup or touchend.
 // 事件触发后的相关信息. mousedown或touchstart时创建, mouseup或touchend后销毁.
@@ -302,108 +314,147 @@ export interface Store extends InitialStore {
   endEvent: MouseOrTouchEvent
   mouse: EventPosition, // current event position
   initialMouse: EventPosition
-  move: EventPosition2 // Displacement relative to initial position. 相对于初始位置的位移
+  move: EventPosition2 // Moved displacement relative to viewport. 相对于视窗的位移
   movedOrClonedElement: HTMLElement // The element to be moved or cloned.
   movedElement: HTMLElement // The element to be moved.
-  initialPosition: EventPosition // The position relative to offsetParent
+  initialPosition: EventPosition // fixed position. The position relative to viewport
   updateMovedElementStyle: () => void
 }
 // Other type
 // 其他类型
 type EventPosition2 = {x: number, y: number}
 
-/* A mousemove or touchmove event listener generator function. To make specified parent element scroll when drag.
-*/
-export const defaultOptionsForFixScrollBox = {
-  triggerMargin: 50,
-  triggerOutterMargin: 30,
-  scrollSpeed: 0.3,
-}
-export type OptionsForFixScrollBox = Partial<typeof defaultOptionsForFixScrollBox>
-export const allListeningElementsOfFixScrollBox = new Set()
-const trackMouseOrTouchPositionInstance = trackMouseOrTouchPosition({
-  onEnd() { trackMouseOrTouchPositionInstance.stop() }
-})
-export function fixScrollBox(boxElement: HTMLElement, opt: OptionsForFixScrollBox={}) {
-  objectAssignIfKeyNull(opt, defaultOptionsForFixScrollBox)
-  allListeningElementsOfFixScrollBox.add(boxElement)
-  let stopVerticalScroll, stopHorizontalScroll
-  const onMove = (e: MouseOrTouchEvent, mouse:EventPosition) => {
-    if (!dragging) {
-      return
+// edge scroll
+// 边缘滚动
+let stopHorizontalScroll, stopVerticalScroll
+_edgeScroll.afterMove = (store: Store, opt: Options) => {
+  if (!opt.edgeScroll) {
+    return
+  }
+  const margin = opt.edgeScrollTriggerMargin
+  stopOldScrollAnimation()
+  // get triggerPoint. The point trigger edge scroll.
+  let triggerPoint = {x: store.mouse.clientX, y: store.mouse.clientY}
+  if (opt.edgeScrollTriggerMode === 'top_left_corner') {
+    const vp = hp.getViewportPosition(store.movedElement)
+    triggerPoint = {x: vp.x, y: vp.y}
+  }
+  // find the scrollable parent elements
+  // 寻找可滚动的父系元素
+  let foundHorizontal: HTMLElement, foundVertical: HTMLElement, prevElement: HTMLElement, horizontalDir:'left'|'right',verticalDir:'up'|'down'
+  for (const itemEl of hp.elementsFromPoint(triggerPoint.x, triggerPoint.y)) {
+    
+    if (prevElement && !hp.isDescendantOf(prevElement, itemEl)) {
+      // itemEl is being covered by other elements
+      // itemEl被其他元素遮挡
+      continue
     }
-    if (!trackMouseOrTouchPositionInstance.info.started) {
-      trackMouseOrTouchPositionInstance.start()
-    }
-    // stop old scroll animation
-    // 结束之前的滚动动画
-    if (stopVerticalScroll) {
-      stopVerticalScroll()
-      stopVerticalScroll = null
-    }
-    if (stopHorizontalScroll) {
-      stopHorizontalScroll()
-      stopHorizontalScroll = null
-    }
-    // box element scroll width or height greater than offset width or height
-    // 元素滚动宽高大于offset宽高
-    if (boxElement.scrollHeight > boxElement.offsetHeight || boxElement.scrollWidth > boxElement.offsetWidth) {
-      const boxPosition = hp.getViewportPosition(boxElement)
-      const margin = opt.triggerMargin
-      const outterMargin = opt.triggerOutterMargin
-      // check if leaved box before every frame of scrollTo
-      // 在scrollTo执行每一帧前检查是否已离开box元素
-      const beforeEveryFrame = (count:number) => {
-        if (count === 0) {
-          return
-        }
-        const currentMousePosition = trackMouseOrTouchPositionInstance.info.position
-        // find the box element which mouse hovering
-        let foundBoxElement: HTMLElement
-        let firstElement: HTMLElement
-        for (const itemEl of hp.elementsFromPoint(currentMousePosition.clientX, currentMousePosition.clientY)) {
-          if (!firstElement) {
-            firstElement = itemEl
+    const t = 10 // min scrollable displacement. 最小可滚动距离, 小于此距离不触发滚动.
+    if (!foundHorizontal) {
+      if (itemEl.scrollWidth > itemEl.clientWidth) {
+        const vp = fixedGetViewportPosition(itemEl)
+        if (triggerPoint.x <= vp.left + margin) {
+          if (scrollableDisplacement(itemEl, 'left') > t && isScrollable(itemEl, 'x')) {
+            foundHorizontal = itemEl
+            horizontalDir = 'left'
           }
-          if (allListeningElementsOfFixScrollBox.has(itemEl)) {
-            foundBoxElement = itemEl
-            break
+        } else if (triggerPoint.x >= vp.left + itemEl.clientWidth - margin) {
+          if (scrollableDisplacement(itemEl, 'right') > t && isScrollable(itemEl, 'x')) {
+            foundHorizontal = itemEl
+            horizontalDir = 'right'
           }
         }
-        // check if the found element is covered by other elements
-        if (firstElement !== foundBoxElement && !hp.isDescendantOf(firstElement, foundBoxElement)) {
-          foundBoxElement = null
-        }
-        // 
-        if (!foundBoxElement) {
-          return false
+      }
+    }
+    if (!foundVertical) {
+      if (itemEl.scrollHeight > itemEl.clientHeight) {
+        const vp = fixedGetViewportPosition(itemEl)
+        if (triggerPoint.y <= vp.top + margin) {
+          if (scrollableDisplacement(itemEl, 'up') > t && isScrollable(itemEl, 'y')) {
+            foundVertical = itemEl
+            verticalDir = 'up'
+          }
+        } else if (triggerPoint.y >= vp.top + itemEl.clientHeight - margin) {
+          if (scrollableDisplacement(itemEl, 'down') > t && isScrollable(itemEl, 'y')) {
+            foundVertical = itemEl
+            verticalDir = 'down'
+          }
         }
       }
-      // vertical
-      if (boxElement.scrollHeight > boxElement.offsetHeight) {
-        if (mouse.clientY <= boxPosition.top + margin) {
-          stopVerticalScroll = scrollTo({y: 0, element: boxElement, beforeEveryFrame, duration: boxElement.scrollTop / opt.scrollSpeed})
-        } else if (mouse.clientY >= boxPosition.top + boxPosition.height - margin) {
-          stopVerticalScroll = scrollTo({y: boxElement.scrollHeight, element: boxElement, beforeEveryFrame, duration: (boxElement.scrollHeight - boxElement.offsetHeight - boxElement.scrollTop) / opt.scrollSpeed})
-        }
-      }
-      // horizontal
-      if (boxElement.scrollWidth > boxElement.offsetWidth) {
-        if (mouse.clientX <= boxPosition.left + margin) {
-          stopHorizontalScroll = scrollTo({x: 0, element: boxElement, beforeEveryFrame, duration: boxElement.scrollLeft / opt.scrollSpeed})
-        } else if (mouse.clientX >= boxPosition.left + boxPosition.width - margin) {
-          stopHorizontalScroll = scrollTo({x: boxElement.scrollWidth, element: boxElement, beforeEveryFrame, duration: (boxElement.scrollWidth - boxElement.offsetWidth - boxElement.scrollLeft) / opt.scrollSpeed})
-        }
-      }
+    }
+    if (foundHorizontal && foundVertical) {
+      break
+    }
+    prevElement = itemEl
+  }
+  // scroll
+  if (foundHorizontal) {
+    if (horizontalDir === 'left') {
+      stopHorizontalScroll = scrollTo({x: 0, element: foundHorizontal, duration: scrollableDisplacement(foundHorizontal, 'left') / opt.edgeScrollSpeed})
+    } else {
+      stopHorizontalScroll = scrollTo({x: foundHorizontal.scrollWidth - foundHorizontal.clientWidth, element: foundHorizontal, duration: scrollableDisplacement(foundHorizontal, 'right') / opt.edgeScrollSpeed})
     }
   }
-  DragEventService.on(boxElement, 'move', onMove)
-  return {
-    options: opt,
-    destroy: () => {
-      DragEventService.off(boxElement, 'move', onMove)
-      allListeningElementsOfFixScrollBox.delete(boxElement)
+  if (foundVertical) {
+    if (verticalDir === 'up') {
+      stopVerticalScroll = scrollTo({y: 0, element: foundVertical, duration: scrollableDisplacement(foundVertical, 'up') / opt.edgeScrollSpeed})
+    } else {
+      stopVerticalScroll = scrollTo({y: foundVertical.scrollHeight - foundVertical.clientHeight, element: foundVertical, duration: scrollableDisplacement(foundVertical, 'down') / opt.edgeScrollSpeed})
     }
+  }
+  // is element scrollable in a direction
+  // 元素某方向是否可滚动
+  function isScrollable(el:HTMLElement, dir:'x'|'y') {
+    const style = getComputedStyle(el)
+    const key = `overflow-${dir}`
+    // document.documentElement is special
+    const special = document.scrollingElement || document.documentElement
+    if (el === special) {
+      return style[key] === 'visible' || style[key] === 'auto' || style[key] === 'scroll'
+    }
+    return style[key] === 'auto' || style[key] === 'scroll'
+  }
+  // scrollable displacement of element  in a direction
+  // 元素某方向可滚动距离
+  function scrollableDisplacement(el:HTMLElement, dir: 'up'|'down'|'left'|'right') {
+    if (dir === 'up') {
+      return el.scrollTop
+    } else if (dir === 'down') {
+      return el.scrollHeight - el.scrollTop - el.clientHeight
+    } else if (dir === 'left') {
+      return el.scrollLeft
+    } else if (dir === 'right') {
+      return el.scrollWidth - el.scrollLeft - el.clientWidth
+    }
+  }
+  function fixedGetViewportPosition(el: HTMLElement) {
+    const r = hp.getViewportPosition(el)
+    // document.documentElement is special
+    const special = document.scrollingElement || document.documentElement
+    if (el === special) {
+      r.top = 0
+      r.left = 0
+    }
+    return r
+  }
+}
+_edgeScroll.afterDrop = (store: Store, opt: Options) => {
+  if (!opt.edgeScroll) {
+    return
+  }
+  stopOldScrollAnimation()
+}
+
+// stop old scroll animation
+// 结束之前的滚动动画
+function stopOldScrollAnimation() {
+  if (stopHorizontalScroll) {
+    stopHorizontalScroll()
+    stopHorizontalScroll = null
+  }
+  if (stopVerticalScroll) {
+    stopVerticalScroll()
+    stopVerticalScroll = null
   }
 }
 
@@ -465,7 +516,7 @@ function scrollTo(options) {
     count++
   }
   const stop = () => {
-    cancelAnimationFrame(requestAnimationFrameId)    
+    cancelAnimationFrame(requestAnimationFrameId)
   }
   animateScroll()
   // return stop
@@ -482,3 +533,4 @@ function easeInOutQuad(startValue, changeInValue, changedTime, duration) {
   t--;
   return -c/2 * (t*(t-2) - 1) + b;
 }
+
